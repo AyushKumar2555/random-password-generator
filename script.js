8db7e6a62b7e8c515944d72b7aad2b1d781abff4
Ayush kumar <124113566+AyushKumar2555@users.noreply.github.com>
2025-07-30 14:12:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const upperSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet="abcdefghijklmnopqrstuvwxyz"
+const numberSet="123456789"
+const symbolSet = "~!@#$%^&*()_+/"
+const getRandomData = (dataset) => {
+    return dat 
+}
